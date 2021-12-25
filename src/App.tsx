6a005ab8ef79eefdca8d531274c4b810eba75a02
望月祐汰 <mczk9402@gmail.com>
2021-12-25 16:02:21 +0900

    🐛 スタイル調整

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import 'App.css';
 import { Row } from 'components/Row';
-import { requests } from 'request';
+import { requests } from 'request.js';
+import { Banner } from 'components/Banner';
 
 function App() {
   return (
     <div className="App">
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.feachNetflixOriginals} />
+      <Banner />
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.feachNetflixOriginals} isLargeRow />
+      <Row title="Action Movies" fetchUrl={requests.feactActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.feactComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.feactHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.feactRomanceMovies} />
+      <Row title="Document Movies" fetchUrl={requests.feactDocumentMovies} />
     </div>
   );
 }
